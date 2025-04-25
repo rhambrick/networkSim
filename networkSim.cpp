07a88eecedd3a227c2163c1e4d94769da6ebde8b
rhambrick <rhambrick10@gmail.com>
2025-04-25 17:27:35 -0500

    continue with CA routing table

--- networkSim.cpp
+++ networkSim.cpp
@@ -1,10 +1,9 @@
-// See whiteboard for system map.
+// See whiteboard for system map. Note that IPs are truncated to the first 8 bits: 192.168.0.0 is just 192.
 // Compile in terminal with: clang++ networkSim.cpp -o out
 
 /*
 	Steps: 1) local devices connect to their local routers
-	2) router routing tables initialized surface level
-	2.5) router tables fully mapped (need to figure this out on paper)
+	2) router routing tables initialized (this is manually done on a small scale, but in real life this is automated via sharing)
 	3) Terminal outputs frame format, user can address stuff and indicates data to send, output is human readable
 	4) each hop is printed out to terminal
 	5) once data is recieved, other computer acks by "sending it back", just printing it.
@@ -37,20 +36,26 @@ class Machine {
 class Router {
 	private:
 		string name;
+		int IP;
 		map<int, int> routingTable;	// Routing table: IP, port
 		map<int, int> connectedDevices; // List of who's connected: index, MAC
 		int numConnectedDevices;
 	public:
-		void setRouterName(string n) {
+		void setRouter(int i, string n) {
 			name = n;
+			IP = i;
 		}
 		void connectMachineToRouter(Machine m) {
 			connectedDevices.insert(make_pair(numConnectedDevices++,m.getDeviceMAC()));
 			cout << "MAC " << hex << m.getDeviceMAC() << " successfully connected to " << name << endl;
 		}
+		void addRoute (int IP, int port) {
+			routingTable.insert(make_pair(IP,port));
+		}
   };
   
   int main() {
+	// Initialize machines and their MACs
     Machine localUser;
 	Machine localA;
 	Machine localB;
@@ -71,25 +76,38 @@ class Router {
 	NY1.setDevice(0xD2E6, "NY server 1");
 	NY2.setDevice(0x70B3, "NY server 2");
   
-    Router localRouter;
+    // Initialize device connections to their local routers
 	Router TX;
 	Router CA;
 	Router IL;
 	Router FL;
 	Router NY;
 
-	localRouter.setRouterName("Local Router");
-	localRouter.connectMachineToRouter(localUser);
-	localRouter.connectMachineToRouter(localA);
-	localRouter.connectMachineToRouter(localB);
+	TX.setRouter(245, "Local Router, TX");
+	TX.connectMachineToRouter(localUser);
+	TX.connectMachineToRouter(localA);
+	TX.connectMachineToRouter(localB);
 
-	CA.setRouterName("CA Router");
+	CA.setRouter(164, "CA Router");
 	CA.connectMachineToRouter(CA1);
 	CA.connectMachineToRouter(CA2);
 
-	NY.setRouterName("NY Router");
+	NY.setRouter(165, "NY Router");
 	NY.connectMachineToRouter(NY1);
 	NY.connectMachineToRouter(NY2);
+
+	IL.setRouter(92, "IL Router");
+	FL.setRouter(218, "FL Router");
+
+	// Initialize routing tables
+	TX.addRoute(0xA3F1, 3.1);
+	TX.addRoute(0x2B8D, 3.2);
+	TX.addRoute(0xC509, 3.3);
+	TX.addRoute(164, 1);
+	TX.addRoute(92, 2);
+	TX.addRoute(218, 4);
+
+	CA.addRoute(0xE76A, 1);
 
     return 0;
   }
